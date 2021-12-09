49352863e8f292676acd06477397d8fd1470ec40
Mai Hường <maihuongyp27@gmail.com>
2021-12-09 19:48:50 +0700

    Week 2

---
+++ Lab7/AddTwoNumber.js
@@ -0,0 +1,13 @@
+sum = 10 + 10;
+sub = 12 - 0;
+mul = 10 * 8;
+quo = 66/33;
+
+myVar = 87;
+myVar++;
+
+console.log(sum);
+console.log(sub);
+console.log(mul);
+console.log(quo);
+console.log(myVar);
